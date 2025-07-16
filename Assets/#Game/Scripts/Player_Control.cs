774ecb964ec046f3da03bc72603722ec092bf3c2
M. Shafa N. <142976367+ItsPong@users.noreply.github.com>
2025-07-16 20:58:03 +0700

    movement shafa

--- Assets/#Game/Scripts/Player_Control.cs
+++ Assets/#Game/Scripts/Player_Control.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class Player_Control : MonoBehaviour
 {
     [SerializeField] public float moveSpeed;
     public float jumpForce = 5f;
+    // Variabel untuk mengontrol gravitasi saat jatuh
+    public float fallMultiplier = 2.5f;
+
     private Vector2 movement;
     private Animator anim;
     private SpriteRenderer flip;
     private Rigidbody2D rb;
     private bool isGrounded = false;
+
+    // Variabel untuk menyimpan gravitasi dasar
+    private float baseGravityScale;
 
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.1f;
@@ -20,35 +27,54 @@ public class Player_Control : MonoBehaviour
         anim = GetComponent<Animator>();
         flip = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        // Simpan nilai gravitasi awal dari Rigidbody2D
+        baseGravityScale = rb.gravityScale;
     }
+
     private void Update()
     {
         movement.x = Input.GetAxis("Horizontal");
 
-        //set run untuk transisi animasi
-        anim.SetFloat("Speed", Mathf.Abs(movement.magnitude * moveSpeed));
+        // Set run untuk transisi animasi
+        anim.SetFloat("Speed", Mathf.Abs(movement.x * moveSpeed));
 
         // Flip karakter
         if (movement.x > 0) flip.flipX = false;
         else if (movement.x < 0) flip.flipX = true;
 
-        // Cek loncat
+        // Cek daratan
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         anim.SetBool("IsJumping", !isGrounded);
 
-        // Tombol lompat
+        // Tombol lompat DITEKAN (memulai lompatan)
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
+        // >> LOGIKA VARIABLE JUMP DITAMBAHKAN DI SINI <<
+        // Jika tombol lompat DILEPAS saat sedang naik, potong ketinggian lompatan
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
     }
+
     void FixedUpdate()
     {
-        if (movement != Vector2.zero)
+        // 1. Gerakan Player menggunakan velocity
+        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+
+        // 2. Logika Faster Fall
+        if (rb.velocity.y < 0)
         {
-            var xMovement = movement.x * moveSpeed * Time.deltaTime;
-            this.transform.Translate(new Vector3(xMovement, 0));
+            // Saat jatuh, tingkatkan gravitasi
+            rb.gravityScale = baseGravityScale * fallMultiplier;
+        }
+        else
+        {
+            // Saat naik atau di darat, kembalikan gravitasi ke normal
+            rb.gravityScale = baseGravityScale;
         }
     }
 }
